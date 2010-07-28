2c389c543118a4ca66798c40999d3d96acce2015
Mark Shroyer <code@markshroyer.com>
2010-07-28 18:24:28 -0400

    Fleshed out the externals script
    Added an interactive menu to the externals script, allowing the user to
    select which particular external resources he wants installed.  Also fixed
    the specification of CVS branches.

--- bin/externals.pl
+++ bin/externals.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Clone or update remote dependencies.  This script uses relative paths, so
+# Checkout or update remote dependencies.  This script uses relative paths, so
 # only run this from directly within the .emacs.d directory!
 
 use warnings;
@@ -31,32 +31,42 @@ our @externals = (
     {
         path   => 'elisp/slime',
         vcs    => 'cvs',
-        repo   => ':pserver:anonymous:anonymous@common-lisp.net:/project/slime/cvsroot',
-        branch => 'slime',
+        repo   => ':pserver:anonymous:anonymous@common-lisp.net:/project/slime/cvsroot slime',
+        branch => 'trunk',
     },
 );
 
-sub vcs_cvs_clone {
+sub vcs_cvs_checkout {
     my ($path, $repo, $branch) = @_;
+
+    my @repo_words = split(/ /, $repo);
+    my $cvsroot = $repo_words[0];
+    my $module = $repo_words[1];
+
+    my $branchopt = ( $branch eq 'trunk' ) ? "" : "-r ${branch}";
 
     my $ppath = dirname($path);
     my $oldcwd = getcwd;
     mkdir $ppath unless ( -d $ppath );
     chdir $ppath;
-    `cvs -d ${repo} checkout -P ${branch}`;
+    `cvs -d ${cvsroot} checkout -P ${branchopt} ${module}`;
     chdir $oldcwd;
 }
 
 sub vcs_cvs_update {
     my ($path, $repo, $branch) = @_;
 
+    my @repo_words = split(/ /, $repo);
+    my $cvsroot = $repo_words[0];
+    my $module = $repo_words[1];
+
     my $oldcwd = getcwd;
     chdir $path;
-    `cvs -d ${repo} update -Pd`;
+    `cvs update -Pd`;
     chdir $oldcwd;
 }
 
-sub vcs_git_clone {
+sub vcs_git_checkout {
     my ($path, $repo, $branch) = @_;
 
     `git clone "${repo}" "${path}"`;
@@ -88,24 +98,90 @@ sub get_vcs_cmd {
     else {
         return 0;
     }
-};
+}
 
-for my $ext ( @externals ) {
-    my $fun = undef;
+if ( $^O eq 'MSWin32' ) {
+    print <<"EOF";
 
-    if ( -d $ext->{path} ) {
-        print "\nUpdating " . $ext->{path} . "\n";
-        $fun = get_vcs_cmd($ext->{vcs}, 'update');
-    }
-    else {
-        print "\nCloning " . $ext->{path} . "\n";
-        $fun = get_vcs_cmd($ext->{vcs}, 'clone');
-    }
+WARNING: Checking out from remote CVS repositories using CVSNT can cause
+things to blow up.  In particular, CVSNT mangles the line endings in
+checkouts from the Slime repository, resulting in errors in the Emacs
+startup script.
 
-    if ( $fun ) {
-        &$fun($ext->{path}, $ext->{repo}, $ext->{branch});
+If possible, run this script from within Cygwin instead.
+EOF
+}
+
+my $command = shift @ARGV || '';
+if ( $command eq 'checkout' || $command eq 'co' ) {
+    print <<"EOF";
+
+### Optional external Emacs resources are listed below ###
+
+Externals marked with [i] are alread installed.  Enter an external's number to
+mark it for installation, then type x to install marked externals or q to quit
+without applying any changes.
+EOF
+    do {
+        print "\n";
+        for ( my $i = 0; $i <= $#externals; $i++ ) {
+            my $ext = $externals[$i];
+            my $flag = ( -d $ext->{path} ) ? 'i' :
+                    ( exists $ext->{install} ? '+' : ' ' );
+            printf "%2d. [%s] %s (%s %s)\n", $i+1, $flag,
+                    $ext->{path}, $ext->{vcs}, $ext->{branch};
+        }
+        print "\next> ";
+        my $cmd = <STDIN>;
+        chomp $cmd;
+
+        if ( lc($cmd) eq 'q' ) {
+            exit 0;
+        } elsif ( lc($cmd) eq 'x' ) {
+            for my $ext ( @externals ) {
+                if ( exists $ext->{install} ) {
+                    print "\n### Checking out " . $ext->{path} . " ###\n";
+                    my $fun = get_vcs_cmd($ext->{vcs}, 'checkout');
+                    if ( $fun ) {
+                        &$fun($ext->{path}, $ext->{repo}, $ext->{branch});
+                    } else {
+                        print "Error: No checkout function for "
+                                . $ext->{vcs} . ".\n";
+                    }
+                }
+            }
+            exit 0;
+        } elsif ( $cmd =~ m/^[1-9]\d*$/o && $cmd <= $#externals + 1 ) {
+            my $ext = $externals[$cmd-1];
+            if ( -d $ext->{path} ) {
+                print "\nThe external " . $ext->{path}
+                        . " is already installed.\n";
+            } elsif ( exists $ext->{install} ) {
+                delete $ext->{install};
+            } else {
+                $ext->{install} = 1;
+            }
+        } else {
+            print "\nEnter a number between 1 and "
+                    . ( $#externals + 1 ) . ", x, or q.\n";
+        }
+    } while ( 1 );
+}
+elsif ( $command eq 'update' || $command eq 'up' ) {
+    for my $ext ( @externals ) {
+        if ( -d $ext->{path} ) {
+            print "\n### Updating " . $ext->{path} . " ###\n";
+            my $fun = get_vcs_cmd($ext->{vcs}, 'update');
+            if ( $fun ) {
+                &$fun($ext->{path}, $ext->{repo}, $ext->{branch});
+            }
+            else {
+                print "Error: No update function for " . $ext->{vcs} . ".\n";
+            }
+        }
     }
-    else {
-        print "No supporting VCS function for " . $ext->{path} . "\n";
-    }
+}
+else {
+    print "\nUsage: `$0 checkout` or `$0 update`.\n";
+    exit 1;
 }
