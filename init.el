8e71b46396ff9098f57ab39f1c287233f2c03961
Mark Shroyer <code@markshroyer.com>
2011-04-05 02:14:31 -0400

    Add Solarized color theme ports
    From https://gist.github.com/888286

--- init.el
+++ init.el
@@ -257,13 +257,14 @@
 (transient-mark-mode 0)
 
 ;; Color themes!  (But only when running in a GUI, of course...)
-(if (and window-system
+(when (and window-system
          (boundp 'color-theme-local)
          (not (null color-theme-local)))
-    (progn
-      (require 'color-theme)            ; Only load themes if one was
-      (color-theme-initialize)          ; defined in ~/.emacs.local
-      (funcall color-theme-local)))
+
+  (require 'color-theme)            ; Only load themes if one was
+  (load "color-theme-solarized.el")
+  (color-theme-initialize)          ; defined in ~/.emacs.local
+  (funcall color-theme-local))
 
 ;; Don't mess around with this disabled commands nonsense
 (setq disabled-command-hook nil)
