c5509328f33a4c48d8fe6857669e146896287aa7
Mark Shroyer <code@markshroyer.com>
2012-01-11 13:19:25 -0500

    Reorganize Org Mode global settings
    Reorganize Org Mode settings in init.el so that we can set
    org-directory locally in .emacs.local.el and let the global
    configuration take care of the rest.

--- init.el
+++ init.el
@@ -395,144 +395,6 @@
 (defvar ctags-program-name "ctags")
 
 
-;;; ORG MODE/DIARY
-
-(defun mshroyer/org-todo-active-p ()
-  "Determines whether the current todo item is active
-
-Returns non-nil if the todo item currently under the point can
-currently be worked on; returns nil if the item is blocked from
-completion by either dependency on another todo item or because
-it is scheduled at a future timestamp."
-  (let* ((subtree-end (save-excursion
-                        (org-end-of-subtree t)))
-         (scheduled-time (save-excursion
-                           (if (re-search-forward org-scheduled-time-regexp
-                                                  subtree-end t)
-                               (org-time-string-to-time (match-string 1))
-                             nil)))
-         (time> (lambda (a b)
-                  (cond ((not (car a))       nil)
-                        ((not (car b))       t)
-                        ((< (car a) (car b)) nil)
-                        ((> (car a) (car b)) t)
-                        (t (funcall time> (cdr a) (cdr b)))))))
-    (save-excursion
-      (and (org-block-todo-from-children-or-siblings-or-parent
-            '(:type todo-state-change :to done))
-           (not (and scheduled-time
-                     (funcall time> scheduled-time (org-current-time))))))))
-
-(defun mshroyer/org-show-unblocked-todo-tree ()
-  "Show currently unblocked action items
-
-Builds a sparse tree which highlights only action items which are
-not blocked by other tasks and which are not scheduled into the
-future."
-  (interactive)
-  (org-occur (concat "^" org-outline-regexp " *" org-not-done-regexp)
-             nil
-             'mshroyer/org-todo-active-p))
-
-(define-key org-mode-map "\C-cb" 'mshroyer/org-show-unblocked-todo-tree)
-
-(setq org-agenda-include-diary t
-      org-enforce-todo-dependencies t
-      org-agenda-dim-blocked-tasks nil
-      org-agenda-todo-ignore-scheduled nil
-      org-agenda-remove-tags 'prefix
-      org-deadline-warning-days 7
-      org-stuck-projects '("+LEVEL=1/-DONE"
-                           ("TODO" "WAIT")
-                           nil
-                           nil)
-
-      org-agenda-custom-commands
-      '(("a" agenda "Agenda")
-        ("w" "Waiting-for items by context" todo "WAIT"
-         ((org-agenda-sorting-strategy '(todo-state-up tag-up time-up))
-          (org-agenda-prefix-format "")
-          (org-agenda-todo-keyword-format "")))
-        ("d" "Non-dated action items by context" todo "TODO"
-         ((org-agenda-sorting-strategy '(todo-state-up tag-up time-up))
-          (org-agenda-prefix-format "%16T:")
-          (org-agenda-todo-keyword-format "")
-          (org-agenda-skip-function
-           ;;
-           ;; Hide TODO entries where either:
-           ;;
-           ;;  1. The task is blocked by another not done task, or
-           ;;
-           ;;  2. The task is scheduled with a timestamp still in the
-           ;;     future
-           ;;
-           ;; We use a custom skip function rather than
-           ;; org-agenda-dim-blocked-tasks for condition 1 because we still
-           ;; want to show blocked deadline tasks on the agenda view (and
-           ;; the 'invisible option isn't available in Emacs 23's Org
-           ;; Mode); likewise we need custom functionality for condition 2
-           ;; rather than setting org-agenda-todo-ignore-scheduled to
-           ;; 'future because this considers a task scheduled for any time
-           ;; on the current date as "present" (and 'future isn't available
-           ;; in Emacs 23's Org Mode either).
-           ;;
-           (lambda ()
-             (let* ((subtree-end (save-excursion
-                                   (org-end-of-subtree t))))
-               (if (mshroyer/org-todo-active-p)
-                   nil
-                 subtree-end))))))))
-
-;; Setup org capture, but only if we're using a newer version of Org Mode
-;; that includes this feature...
-(when (require 'org-capture nil t)
-  (setq org-default-notes-file (concat org-directory "/inbox.org")
-        org-capture-templates
-        '(("i" "Inbox" entry (file (concat org-directory "/inbox.org"))
-           "* %?\n"))
-        org-refile-use-outline-path t
-        org-outline-path-complete-in-steps nil
-        org-refile-targets
-        '((("~/org/todo.org") . (:level . 1))))
-  (define-key global-map "\C-cc" 'org-capture))
-
-; Only show holidays that I actually care about
-(setq calendar-holidays
-      '((holiday-fixed 1 1 "New Year's Day")
-        (holiday-float 1 1 3 "Martin Luther King Day")
-        (holiday-fixed 2 2 "Groundhog Day")
-        (holiday-fixed 2 14 "Valentine's Day")
-        (holiday-float 2 1 3 "President's Day")
-        (holiday-fixed 3 17 "St. Patrick's Day")
-        (holiday-fixed 4 1 "April Fools' Day")
-        (holiday-float 5 0 2 "Mother's Day")
-        (holiday-float 5 1 -1 "Memorial Day")
-        (holiday-fixed 6 14 "Flag Day")
-        (holiday-float 6 0 3 "Father's Day")
-        (holiday-fixed 7 4 "Independence Day")
-        (holiday-float 9 1 1 "Labor Day")
-        (holiday-float 10 1 2 "Columbus Day")
-        (holiday-fixed 10 31 "Halloween")
-        (holiday-fixed 11 11 "Veteran's Day")
-        (holiday-float 11 4 4 "Thanksgiving")
-        (holiday-easter-etc)
-        (holiday-fixed 12 25 "Christmas")
-        (holiday-chinese-new-year)
-        (solar-equinoxes-solstices)
-        (holiday-sexp calendar-daylight-savings-starts
-                      (format "Daylight Saving Time Begins %s"
-                              (solar-time-string
-                               (/ calendar-daylight-savings-starts-time
-                                  (float 60))
-                               calendar-standard-time-zone-name)))
-        (holiday-sexp calendar-daylight-savings-ends
-                      (format "Daylight Saving Time Ends %s"
-                              (solar-time-string
-                               (/ calendar-daylight-savings-ends-time
-                                  (float 60))
-                               calendar-daylight-time-zone-name)))))
-
-
 ;;; SLIME
 
 ;; Use Paredit in Inferior SLIME
@@ -584,11 +446,155 @@ future."
   (funcall color-theme-local))
 
 
+;;; ORG MODE / DIARY
+
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(global-set-key "\C-ca" 'org-agenda)
+
+(setq org-agenda-files (mapcar (lambda (file)
+                                 (concat org-directory file))
+                               '("/todo.org" "/archive.org"))
+      diary-file (concat org-directory "/diary")
+      org-agenda-include-diary t
+      org-enforce-todo-dependencies t
+      org-agenda-dim-blocked-tasks nil
+      org-agenda-todo-ignore-scheduled nil
+      org-agenda-remove-tags 'prefix
+      org-deadline-warning-days 7
+      org-stuck-projects '("+LEVEL=1/-DONE"
+                           ("TODO" "WAIT")
+                           nil
+                           nil)
+
+      org-agenda-custom-commands
+      '(("a" agenda "Agenda")
+        ("w" "Waiting-for items by context" todo "WAIT"
+         ((org-agenda-sorting-strategy '(todo-state-up tag-up time-up))
+          (org-agenda-prefix-format "")
+          (org-agenda-todo-keyword-format "")))
+        ("d" "Non-dated action items by context" todo "TODO"
+         ((org-agenda-sorting-strategy '(todo-state-up tag-up time-up))
+          (org-agenda-prefix-format "%16T:")
+          (org-agenda-todo-keyword-format "")
+          (org-agenda-skip-function
+           ;;
+           ;; Hide TODO entries where either:
+           ;;
+           ;;  1. The task is blocked by another not done task, or
+           ;;
+           ;;  2. The task is scheduled with a timestamp still in the
+           ;;     future
+           ;;
+           ;; We use a custom skip function rather than
+           ;; org-agenda-dim-blocked-tasks for condition 1 because we still
+           ;; want to show blocked deadline tasks on the agenda view (and
+           ;; the 'invisible option isn't available in Emacs 23's Org
+           ;; Mode); likewise we need custom functionality for condition 2
+           ;; rather than setting org-agenda-todo-ignore-scheduled to
+           ;; 'future because this considers a task scheduled for any time
+           ;; on the current date as "present" (and 'future isn't available
+           ;; in Emacs 23's Org Mode either).
+           ;;
+           (lambda ()
+             (let* ((subtree-end (save-excursion
+                                   (org-end-of-subtree t))))
+               (if (mshroyer/org-todo-active-p)
+                   nil
+                 subtree-end))))))))
+
+(defun mshroyer/org-todo-active-p ()
+  "Determines whether the current todo item is active
+
+Returns non-nil if the todo item currently under the point can
+currently be worked on; returns nil if the item is blocked from
+completion by either dependency on another todo item or because
+it is scheduled at a future timestamp."
+  (let* ((subtree-end (save-excursion
+                        (org-end-of-subtree t)))
+         (scheduled-time (save-excursion
+                           (if (re-search-forward org-scheduled-time-regexp
+                                                  subtree-end t)
+                               (org-time-string-to-time (match-string 1))
+                             nil)))
+         (time> (lambda (a b)
+                  (cond ((not (car a))       nil)
+                        ((not (car b))       t)
+                        ((< (car a) (car b)) nil)
+                        ((> (car a) (car b)) t)
+                        (t (funcall time> (cdr a) (cdr b)))))))
+    (save-excursion
+      (and (org-block-todo-from-children-or-siblings-or-parent
+            '(:type todo-state-change :to done))
+           (not (and scheduled-time
+                     (funcall time> scheduled-time (org-current-time))))))))
+
+(defun mshroyer/org-show-unblocked-todo-tree ()
+  "Show currently unblocked action items
+
+Builds a sparse tree which highlights only action items which are
+not blocked by other tasks and which are not scheduled into the
+future."
+  (interactive)
+  (org-occur (concat "^" org-outline-regexp " *" org-not-done-regexp)
+             nil
+             'mshroyer/org-todo-active-p))
+
+(define-key org-mode-map "\C-cb" 'mshroyer/org-show-unblocked-todo-tree)
+
+;; Setup org capture, but only if we're using a newer version of Org Mode
+;; that includes this feature...
+(when (require 'org-capture nil t)
+  (setq org-default-notes-file (concat org-directory "/inbox.org")
+        org-capture-templates
+        '(("i" "Inbox" entry (file (concat org-directory "/inbox.org"))
+           "* %?\n"))
+        org-refile-use-outline-path t
+        org-outline-path-complete-in-steps nil
+        org-refile-targets
+        '((("~/org/todo.org") . (:level . 1))))
+  (define-key global-map "\C-cc" 'org-capture))
+
+; Only show holidays that I actually care about
+(setq calendar-holidays
+      '((holiday-fixed 1 1 "New Year's Day")
+        (holiday-float 1 1 3 "Martin Luther King Day")
+        (holiday-fixed 2 2 "Groundhog Day")
+        (holiday-fixed 2 14 "Valentine's Day")
+        (holiday-float 2 1 3 "President's Day")
+        (holiday-fixed 3 17 "St. Patrick's Day")
+        (holiday-fixed 4 1 "April Fools' Day")
+        (holiday-float 5 0 2 "Mother's Day")
+        (holiday-float 5 1 -1 "Memorial Day")
+        (holiday-fixed 6 14 "Flag Day")
+        (holiday-float 6 0 3 "Father's Day")
+        (holiday-fixed 7 4 "Independence Day")
+        (holiday-float 9 1 1 "Labor Day")
+        (holiday-float 10 1 2 "Columbus Day")
+        (holiday-fixed 10 31 "Halloween")
+        (holiday-fixed 11 11 "Veteran's Day")
+        (holiday-float 11 4 4 "Thanksgiving")
+        (holiday-easter-etc)
+        (holiday-fixed 12 25 "Christmas")
+        (holiday-chinese-new-year)
+        (solar-equinoxes-solstices)
+        (holiday-sexp calendar-daylight-savings-starts
+                      (format "Daylight Saving Time Begins %s"
+                              (solar-time-string
+                               (/ calendar-daylight-savings-starts-time
+                                  (float 60))
+                               calendar-standard-time-zone-name)))
+        (holiday-sexp calendar-daylight-savings-ends
+                      (format "Daylight Saving Time Ends %s"
+                              (solar-time-string
+                               (/ calendar-daylight-savings-ends-time
+                                  (float 60))
+                               calendar-daylight-time-zone-name)))))
+
+
 ;;; EDITING MODE HOOKS AND SETTINGS
 
 ;; Org mode...
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(global-set-key "\C-ca" 'org-agenda)
+
 ; Backtab in terminal emulators such as gnome-terminal, konsole, etc.:
 (define-key org-mode-map "\M-[z" 'org-shifttab)
 ; Use C-ct as an alternative for C-cC-t, so that we don't have to use quite
