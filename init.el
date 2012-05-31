75a6772189c0158b52f70132804b5e5f636ce34e
Mark Shroyer <code@markshroyer.com>
2012-05-31 18:09:58 -0400

    Add interactive what-face function to get font

--- init.el
+++ init.el
@@ -1050,6 +1050,14 @@ future."
   (beginning-of-buffer))
 
 
+;; Yoinked from http://stackoverflow.com/questions/1242352/get-font-face-under-cursor-in-emacs
+(defun what-face (pos)
+  (interactive "d")
+  (let ((face (or (get-char-property (point) 'read-face-name)
+                  (get-char-property (point) 'face))))
+    (if face (message "Face: %s" face) (message "No face at %d" pos))))
+
+
 ;; Generate tags for directory with Exuberant Ctags
 (defun create-tags (dir-name)
   "Create tags file with Exuberant Ctags."
