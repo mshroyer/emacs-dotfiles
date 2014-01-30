b5796b0ca66187021e4069086e0230f822f1e47f
Mark Shroyer <code@markshroyer.com>
2014-01-30 00:33:07 -0500

    Add Marmalade package repository

--- init.el
+++ init.el
@@ -101,6 +101,7 @@
   (load (concat user-elisp-directory "package.el"))
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
