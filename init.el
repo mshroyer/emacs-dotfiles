990024fd0d95aeede3df17da52465bbfe79d8baf
Mark Shroyer <code@markshroyer.com>
2014-11-05 09:48:29 -0800

    Use khaki color to highlight trailing whitespace

--- init.el
+++ init.el
@@ -1767,17 +1767,18 @@ for example.
 
 ;; Custom variables from the Emacs configuration editor
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(LaTeX-verbatim-environments (quote ("verbatim" "verbatim*" "Verbatim")))
  '(ecb-layout-window-sizes nil)
  '(ecb-options-version "2.40")
  '(safe-local-variable-values (quote ((TeX-master . "manual") (TeX-master . t)))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:inherit nil)))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t (:inherit nil))))
+ '(trailing-whitespace ((t (:background "khaki3" :foreground "black")))))
