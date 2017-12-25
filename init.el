30ba6b7b1906656a5fefdbe8357afd7405318146
Mark Shroyer <mark@shroyer.name>
2017-12-25 14:37:56 -0500

    Add mshroyer-lib.el
    Moves some custom functions into a discrete library.

--- init.el
+++ init.el
@@ -151,6 +151,7 @@
 (require 'rust-mode)
 (require 'slime)
 (require 'tomatinho)
+(require 'mshroyer-lib)
 
 ;; Optional features
 
@@ -418,7 +419,7 @@
 ;;; CUSTOM COMMANDS
 
 (global-set-key "\C-cu" 'calc)
-(global-set-key "\C-cm" 'timestamp-insert)
+(global-set-key "\C-cm" 'mshroyer-insert-timestamp)
 (global-set-key "\C-cg" 'create-tags)
 (global-set-key "\C-cp" 'compile)
 (global-set-key "\C-cf" 'auto-fill-mode)
@@ -767,7 +768,7 @@ Recognized window header names are: 'comint, 'locals, 'registers,
             (setq scroll-margin 0)))
 
 ;; Text mode...
-(define-key text-mode-map "\C-cn" 'new-journal-entry)
+(define-key text-mode-map "\C-cn" 'mshroyer-new-journal-entry)
 (define-key text-mode-map "\C-c\C-o" 'org-open-at-point)
 (define-key text-mode-map "\C-ct" 'artist-mode)
 (define-key text-mode-map (kbd "TAB") 'tab-to-tab-stop)
@@ -832,67 +833,6 @@ Recognized window header names are: 'comint, 'locals, 'registers,
   (loop for var in (dump-variables)
         do (insert (format "%s = %s" (car var) (cdr var)))))
 
-;; Show an ASCII table
-;; (Inspired by http://www-cdf.fnal.gov/~sthrlnd/emacs_help.html)
-(defun ascii-table ()
-  "Show a table of ASCII characters by decimal, hex, and octal value."
-
-  (interactive)
-  (switch-to-buffer "*ASCII*")
-  (erase-buffer)
-  (let ((min 1) (max 255)
-        (special-chars '(
-                         (1 . "%c  SOH (start of heading)")
-                         (2 . "%c  STX (start of text)")
-                         (3 . "%c  ETX (end of text)")
-                         (4 . "%c  EOT (end of transmission)")
-                         (5 . "%c  ENQ (enquiry)")
-                         (6 . "%c  ACK (acknowledge)")
-                         (7 . "%c  BEL (bell)")
-                         (8 . "%c  BS  (backspace)")
-                         (9 . "    TAB (horizontal tab)")
-                         (10 . "    LF  (NL line feed, new line)")
-                         (11 . "%c  VT  (vertical tab)")
-                         (12 . "    FF  (NP form feed, new page)")
-                         (13 . "%c  CR  (carriage return)")
-                         (14 . "%c  SO  (shift out)")
-                         (15 . "%c  SI  (shift in)")
-                         (16 . "%c  DLE (data link escape)")
-                         (17 . "%c  DC1 (device control 1)")
-                         (18 . "%c  DC2 (device control 2)")
-                         (19 . "%c  DC3 (device control 3)")
-                         (20 . "%c  DC4 (device control 4)")
-                         (21 . "%c  NAK (negative acknowledge)")
-                         (22 . "%c  SYN (synchronous idle)")
-                         (23 . "%c  ETB (end of trans. block)")
-                         (24 . "%c  CAN (cancel)")
-                         (25 . "%c  EM  (end of medium)")
-                         (26 . "%c  SUB (substitute)")
-                         (27 . "%c  ESC (escape)")
-                         (28 . "%c  FS  (file separator)")
-                         (29 . "%c  GS  (group separator)")
-                         (30 . "%c  RS  (record separator)")
-                         (31 . "%c  US  (unit separator)")
-                         (32 . "%c       (space)")
-                         (9999)
-                         )))
-    (insert (format "ASCII characters %d-%d\n\n" min max))
-    (insert " Dec   Hex   Oct    Character\n")
-    (let ((i 0))
-      (while (< i 60)
-        (insert "=")
-        (setq i (+ i 1))))
-    (insert "\n")
-    (let ((i min))
-      (while (<= i max)
-        (let ((line "%4d  0x%02X  %04o    ") (char "%c"))
-          (while (> i (car (car special-chars)))
-            (setq special-chars (cdr special-chars)))
-          (if (= (car (car special-chars)) i)
-              (setq char (cdr (car special-chars))))
-          (insert (format (concat line char "\n") i i i i))
-          (setq i (+ i 1))))))
-  (beginning-of-buffer))
 
 
 ;; Yoinked from http://stackoverflow.com/questions/1242352/get-font-face-under-cursor-in-emacs
@@ -925,67 +865,6 @@ format returned by (format-time-string \"%z\" now).
       (setq minutes (* minutes -1)
             sign    "-"))
     (concat sign (format "%02d%02d" (floor minutes 60) (mod minutes 60)))))
-
-
-(defun timestamp-string ()
-  "Returns a Unix date(1)-format timestamp
-
-Will try figure out the timezone name from your
-calendar-standard-time-zone-name and
-calendar-daylight-time-zone-name variables if the system doesn't
-return a time zone name -- as with NT Emacs as of version 23.2,
-for example.
-"
-
-  (let* ((now (current-time))
-         (str-date (format-time-string "%a %b %e" now))
-         (str-time (format-time-string "%H:%M:%S" now))
-         (sys-tz   (format-time-string "%Z" now))
-         (str-year (format-time-string "%Y" now))
-         (str-tz (if (> (length sys-tz) 0)
-                     sys-tz
-                     (let ((off-tz (format-time-string "%z" now)))
-                       (cond ((equal off-tz
-                                     (calendar-zone-to-tz-offset
-                                      calendar-time-zone))
-                              calendar-standard-time-zone-name)
-
-                             ((equal off-tz
-                                     (calendar-zone-to-tz-offset
-                                      (+ 60 calendar-time-zone)))
-                              calendar-daylight-time-zone-name)
-
-                             (t
-                              nil))))))
-    (if str-tz
-        (concat str-date " " str-time " " str-tz " " str-year)
-        (concat str-date " " str-time " " str-year))))
-
-
-(defun timestamp-insert ()
-  "Inserts a Unix date(1)-format timestamp in the current buffer"
-
-  (interactive)
-  (insert (timestamp-string)))
-
-
-;; Create a new journal entry
-(defun new-journal-entry ()
-  "Make a new journal entry with a Unix `date`-style timestamp"
-
-  (interactive)
-  (end-of-buffer)
-  (if (re-search-backward "[^ \t\n]" nil t)
-      (progn
-        (end-of-line)
-        (let ((beg (point)))
-          (end-of-buffer)
-          (delete-region beg (point)))
-        (dotimes (i 3)
-          (newline))))
-  (insert (timestamp-string))
-  (dotimes (i 2)
-    (newline)))
 
 
 ;; Consolidate flyspell commands
