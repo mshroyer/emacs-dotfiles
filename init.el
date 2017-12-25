e949070e4fafc0e10873ee228ca196104a797e4c
Mark Shroyer <mark@shroyer.name>
2017-12-24 22:18:17 -0500

    Remove GUI prompt suppression for MacOS
    Emacs no longer seems to produce graphical prompts on MacOS by default, and
    the functions I had been advising no longer seem to exist.

--- init.el
+++ init.el
@@ -316,21 +316,6 @@
 ;; Add a keystroke for renaming a buffer
 (global-set-key "\C-cr" 'rename-buffer)
 
-;; Don't use graphical confirmation dialogs; these cause a number of
-;; problems, including hanging Emacs when we try to perform a system
-;; shutdown in OS X.
-;;
-;; Borrowed from:
-;; http://superuser.com/questions/125569/how-to-fix-emacs-popup-dialogs-on-mac-os-x
-(defadvice yes-or-no-p (around prevent-dialog activate)
-  "Prevent yes-or-no-p from activating a dialog"
-  (let ((use-dialog-box nil))
-    ad-do-it))
-(defadvice y-or-n-p (around prevent-dialog-yorn activate)
-  "Prevent y-or-n-p from activating a dialog"
-  (let ((use-dialog-box nil))
-    ad-do-it))
-
 ;; Don't limit expression evaluation output
 (setq eval-expression-print-length nil
       eval-expression-print-level  nil)
