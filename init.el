62246e376ac7c7685e8067d485d544d7168a4367
Mark Shroyer <mark@shroyer.name>
2017-12-25 13:57:50 -0500

    Move Diary Mode init into separate file

--- init.el
+++ init.el
@@ -514,6 +514,7 @@ Recognized window header names are: 'comint, 'locals, 'registers,
 ;;; ORG MODE / DIARY
 
 (load-file (concat user-emacs-directory "init-org.el"))
+(load-file (concat user-emacs-directory "init-diary.el"))
 
 
 ;;; ERC
@@ -533,39 +534,6 @@ Recognized window header names are: 'comint, 'locals, 'registers,
           (lambda ()
             (make-local-variable 'scroll-margin)
             (setq scroll-margin 0)))
-
-;; Diary mode...
-(defun diary-range (first &optional last on-sexps off-sexps)
-  "Diary entry for a event spanning over a range of dates."
-  (and
-   (or (null first)
-       (not (calendar-date-compare (list date) (list first))))
-   (or (null last)
-       (not (calendar-date-compare (list last) (list date))))
-   (or (null on-sexps)
-       (eql on-sexps t)
-       (eval (cons 'or (mapcar #'(lambda (d)
-                                   (not (null (eval d)))) on-sexps))))
-   (or (null off-sexps)
-       (not (eval (cons 'or (mapcar #'(lambda (d)
-                                   (not (null (eval d)))) off-sexps)))))
-   (cons diary-marking-entries-flag entry)))
-
-(defun diary-weekdays (&rest args)
-  (if (find (calendar-day-of-week date) args)
-      (cons diary-marking-entries-flag entry)
-    nil))
-
-(global-set-key "\C-cd" 'diary)
-(add-hook 'diary-mode-hook
-          (lambda ()
-            (setq indent-tabs-mode nil)
-            (make-local-variable 'tab-stop-list)
-            (setq tab-stop-list '(4 16))
-            (local-set-key (kbd "TAB") 'tab-to-tab-stop)))
-(setq diary-display-function 'diary-fancy-display)
-(add-hook 'list-diary-entries-hook 'include-other-diary-files)
-(add-hook 'mark-diary-entries-hook 'mark-included-diary-files)
 
 ;; HTML mode...
 (let ((mode (if (featurep 'web-mode) 'web-mode 'html-mode)))
