7976e93592ec0f4db3e76995f4a6712721005001
Mark Shroyer <mark@shroyer.name>
2018-01-05 21:52:52 -0800

    Remove half tab stop from text-mode

--- init.el
+++ init.el
@@ -698,7 +698,7 @@
           (lambda ()
             (make-local-variable 'tab-stop-list)
             (setq tab-width 8
-                  tab-stop-list '(4 8 16 24 32 40 48 56 64 72 80 88 96 104 112 120)
+                  tab-stop-list '(8 16 24 32 40 48 56 64 72 80 88 96 104 112 120)
                   indent-tabs-mode t)
             (auto-fill-mode 1)))
 
