cdedeb9660edecc05a4a38d0d886f2cb23cbcb60
Mark Shroyer <mark@shroyer.name>
2018-05-05 00:34:42 -0700

    Configure helm with use-package

--- init-packages.el
+++ init-packages.el
@@ -9,37 +9,32 @@
 
 (package-initialize)
 
-;; Package configurations.
-(when (require 'helm nil t)
-  (global-set-key "\C-ch" 'helm-command-prefix)
-  (helm-mode 1)
-  (global-set-key "\C-xb" 'helm-mini)
-  (global-set-key "\C-x\C-f" 'helm-find-files)
-
-  ;; Automatically scale helm window to fit its contents.
-  (helm-autoresize-mode t)
-
-  ;; Increase the width of the buffer name column.
-  (setq helm-buffer-max-length 32)
-
-  ;; Don't use helm for M-x.
-  (add-to-list 'helm-completing-read-handlers-alist '(execute-extended-command . nil))
-  (add-to-list 'helm-completing-read-handlers-alist '(ff-find-other-file . nil))
-  (add-to-list 'helm-completing-read-handlers-alist '(woman . nil))
-
-  ;; Additional bindings.
-  (global-set-key "\M-y" 'helm-show-kill-ring)
-  (setq helm-buffers-fuzzy-matching t
-        helm-recentf-fuzzy-match t)
-
-  ;; Show helm at the bottom of the frame
-  (add-to-list 'display-buffer-alist
-               `(,(rx bos "*helm" (* not-newline) "*" eos)
-                 (display-buffer-in-side-window)
-                 (inhibit-same-window . t)
-                 (window-height . 0.4))))
-
 (when (featurep 'use-package)
+  (use-package helm
+    :ensure t
+    :bind (("C-c h" . helm-command-prefix)
+           ("C-x b" . helm-mini)
+           ("C-x C-f" . helm-find-files)
+           ("M-y" . helm-show-kill-ring))
+    :init
+    (setf helm-buffer-max-length 32
+          helm-buffers-fuzzy-matching t
+          helm-recentf-fuzzy-match t)
+    :config
+    (helm-mode 1)
+    (helm-autoresize-mode t)
+
+    ;; Don't use helm for M-x.
+    (add-to-list 'helm-completing-read-handlers-alist '(execute-extended-command . nil))
+    (add-to-list 'helm-completing-read-handlers-alist '(ff-find-other-file . nil))
+    (add-to-list 'helm-completing-read-handlers-alist '(woman . nil))
+
+    ;; Show helm at the bottom of the frame
+    (add-to-list 'display-buffer-alist
+                 `(,(rx bos "*helm" (* not-newline) "*" eos)
+                   (display-buffer-in-side-window)
+                   (inhibit-same-window . t)
+                 (window-height . 0.4))))
 
   (use-package magit
     :ensure t
