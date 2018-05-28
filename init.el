350221320625f4175994af2629d5d0aecdda75b1
Mark Shroyer <mark@shroyer.name>
2018-05-27 18:11:56 -0700

    Use jupyter console for Python interpreter

--- init.el
+++ init.el
@@ -586,6 +586,11 @@
             (make-local-variable 'scroll-margin)
             (setq scroll-margin         0
                   comint-process-echoes t)))
+(setf python-shell-interpreter "jupyter"
+      python-shell-interpreter-args "console --simple-prompt"
+      python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters
+             "jupyter")
 
 ;; Lisp...
 (setq slime-contribs '(slime-fancy))
