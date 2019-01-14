6f3bec6bc4d8569929a8c930ec51e64be567e60e
Mark Shroyer <mark@shroyer.name>
2019-01-14 10:59:36 -0800

    Enable org-babel for shell commands

--- init-org.el
+++ init-org.el
@@ -33,6 +33,9 @@
 ; Don't re-evaluate code blocks when exporting.
 (setq org-export-babel-evaluate nil)
 
+(org-babel-do-load-languages 'org-babel-load-languages
+                             '((shell . t)))
+
 ; Always show context when creating sparse trees:
 (setq org-show-siblings t)
 
