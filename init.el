d5446213cdd1bb74f4fbb472c5d5803169e0f6cf
Mark Shroyer <mark@shroyer.name>
2025-04-03 14:05:31 -0700

    Test commit

--- init.el
+++ init.el
@@ -2,8 +2,8 @@
 
 ;; Intended for use with Emacs 27 or newer.
 
-;;;; Language dependencies.
 
+;;;; Language dependencies.
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
