198cf4d982054f4186a0e41a647aab2d1cdbfaab
Mark Shroyer <mark@shroyer.name>
2026-04-03 09:50:46 -0700

    Don't set tab-stop-list in text-mode hook
    This gets in the way of overriding tab stop settings in file local variables.

--- init.el
+++ init.el
@@ -784,7 +784,6 @@
           (lambda ()
             (make-local-variable 'tab-stop-list)
             (setq tab-width 8
-                  tab-stop-list '(8 16 24 32 40 48 56 64 72 80 88 96 104 112 120)
                   indent-tabs-mode t)
             (auto-fill-mode 1)))
 
