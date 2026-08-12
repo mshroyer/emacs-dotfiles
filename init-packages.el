89e6f5a52d57d754751c9fa71d989fdbbc0dd152
Mark Shroyer <mark@shroyer.name>
2026-08-12 01:17:23 +0000

    [packages] Fix broken sexp in masm-mode setup

--- init-packages.el
+++ init-packages.el
@@ -108,7 +108,7 @@ So I'll just keep using this macro for now. It's cool."
                   :config
 
                   ; masm-colon mapping messes with typing options
-                  (define-key masm-mode-map ":" #'mshroyer/masm-colon)))
+                  (define-key masm-mode-map ":" #'mshroyer/masm-colon))
 
   (ensure-package nasm-mode
                   :init
